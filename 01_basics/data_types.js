1a6aee153d6c8e5ff00ea14504bbc1bb5230a57a
sahilsoni9905 <155446568+sahilsoni9905@users.noreply.github.com>
2024-01-14 16:30:15 +0000

    done of datatype thing

---
+++ 01_basics/data_types.js
@@ -0,0 +1,12 @@
+// alert(2+3) this dosent like this here , as it is node js not browser 
+// data types to sab humlog pehle hi padh chuke h
+let a = null
+let b 
+
+// there is 2 thing i.e null and undefine 
+// undefine -> is the empty value jaise abhi b ka value undefine h
+// null -> ek value h ... hum tb use kar skte h .. jab hume error wagere ka pta lgana hota h 
+
+//*****IMPPORTANT***** */
+console.log(typeof null); //-> gives object
+console.log(typeof undefined); // -> gives undefine
