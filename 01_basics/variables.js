3a9943088db0745efbb7fb82f918ff0c2892705f
sahilsoni9905 <155446568+sahilsoni9905@users.noreply.github.com>
2024-01-14 16:16:01 +0000

    done variables part

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const id = 11122;
+var email = 'sahil@gmail.com'
+let accountPassword = "12345"
+accountCity = "jaipur"
+//id = 2; gives error
+console.log(id);
+email = 'sahil'
+console.table([id , email , accountPassword , accountCity])
+// always use const and let only bcoz var have so many scope problem 
