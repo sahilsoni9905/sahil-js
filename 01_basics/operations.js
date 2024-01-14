54710adb22a37167cc1894a5cab425fccd8edf83
sahilsoni9905 <155446568+sahilsoni9905@users.noreply.github.com>
2024-01-14 18:13:34 +0000

    impo

---
+++ 01_basics/operations.js
@@ -0,0 +1,23 @@
+let value = 3;
+let negValue = -value
+// console.log(negValue); obviously -> -3
+
+console.log(2+2);
+console.log(2**2); // work as 2 power 2
+
+let str1 = 'hello'
+let str2 = 'hitesh'
+let str3 = str1 + str2
+
+/////
+console.log("1" + 2);  // -> 12
+console.log(1 + "2");  // ->12
+console.log('1' + 2 + 2); // -> 122
+console.log(1 + 2 + '2'); // -> 32
+
+//+++++++++++++++++++++++++++++++++
+// THERE ARE 2 WAYS IN WHICH MEMOROY IS STORED
+// 1. STACK ->  used to store primitive data type and when we get a COPY
+// 2. HEAP ->  used to store non primitive and it pass as refference 
+
+
