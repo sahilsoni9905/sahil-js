9a04f9ffcf6ae8c3346e936c94a0e5051a30b60f
sahilsoni9905 <155446568+sahilsoni9905@users.noreply.github.com>
2024-01-17 18:24:54 +0000

    talks about object and json

---
+++ 01_basics/object3.js
@@ -0,0 +1,14 @@
+// now we are going to talk about destructuring 
+const course = {
+    coursename : 'js',
+    price : '999',
+    instructor : 'sahil',
+}
+ // course.instructor this can be also called by
+ const {instructor} = course;
+ console.log(instructor);
+
+ //**************lets coming to api  */
+ /* apna kaam dusre k sar dalna  */
+ /* hammlog jab api calls aati h to uski value json m aati h  */
+ // jason ek object k tarah hi hota h .... bas uski keys bhi string hoti h aur uski values bhi 
