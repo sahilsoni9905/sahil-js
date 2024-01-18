776df8dd1c643b2488c2d45437e54c290c86bdb2
sahilsoni9905 <155446568+sahilsoni9905@users.noreply.github.com>
2024-01-18 15:43:32 +0000

    more about loops of array

---
+++ 01_basics/more_loop.js
@@ -0,0 +1,14 @@
+const codinglang  = ["ruby" , "pythob" , "java" , "cpp"];
+
+// codinglang.forEach(function (item) {
+//     console.log(item);
+// }  ) /*  ->  ruby
+// pythob
+// java
+// cpp */
+
+// or
+
+codinglang.forEach(  (item , index)   => {
+    console.log(item , index);
+})
